bf07f786dc8e8cecd2abcd270d7315d7a903c2fe
gksrivastava05 <gksrivastava05@gmail.com>
2023-01-27 12:21:32 +0530

    change some code

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-var arr = [1, 2,4];
-console.log(arr);
+// var arr = [1, 2,4];
+// console.log(arr);
+
+//reduce() example
+
+let arr = [1, 2, 3];
+
+let result = arr.reduce((acc, value) => {
+    acc = acc + value;
+    return acc
+}, 0);
+
+console.log(result);
